14a4acabd4a7e816884014356d1dbaa90bbe973c
hourin <phucjaki@gmail.com>
2026-01-14 19:30:39 +0700

    Changed: AST printing defaults to STDERR

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main(int argc, char **argv)
 {
         if (yyparse() != 0)
                 return 1;
-        recursive_print(stdout, parser_ast, 0);
+        recursive_print(stderr, parser_ast, 0);
         analyze_and_print(stdout, parser_ast);
 
         return 0;
